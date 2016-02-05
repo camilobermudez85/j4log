b0f2c99a92f84a96ff5ee18e7f4bc121ec77bb50
Camilo Bermúdez <camilobermudez85@gmail.com>
2016-02-04 23:37:07 -0500

    License headers

--- src/main/java/co/huitaca/j4log/plugins/LogBackPlugin.java
+++ src/main/java/co/huitaca/j4log/plugins/LogBackPlugin.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 Camilo Bermúdez
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package co.huitaca.j4log.plugins;
 
 import java.lang.reflect.InvocationTargetException;
@@ -11,7 +26,7 @@ import java.util.WeakHashMap;
 import co.huitaca.j4log.J4LogPlugin;
 import co.huitaca.j4log.LogLevel;
 
-public class LogBackPlugin extends J4LogPlugin{
+public class LogBackPlugin extends J4LogPlugin {
 
 	private static final String LOG4J_LOG_MANAGER = "org.apache.log4j.LogManager";
 	private static final String LOG4J_LOG_MANAGER_GET_CURRENT_LOGGERS = "getCurrentLoggers";
